1dadba08d243debb180fcb6b9efac48faf0d4286
hepzishanthijohn <yyepsipal@gmail.com>
2025-03-04 18:15:09 +0530

    update

--- frontend/src/pages/CustomerTestimonials/CustomerTestimonials.jsx
+++ frontend/src/pages/CustomerTestimonials/CustomerTestimonials.jsx
@@ -1,19 +1,44 @@
 import React from "react";
+import './CustomerTestimonials.css'
+import cam3 from '../../assets/IMAGES/customers/cam3.jpg'
+import cam1 from '../../assets/IMAGES/customers/cam1.jpg'
+import cam2 from '../../assets/IMAGES/customers/cam2.jpeg'
+import meeting1 from '../../assets/IMAGES/customers/meeting1.webp'
+import meeting2 from '../../assets/IMAGES/customers/meeting2.jpg'
+import meeting3 from '../../assets/IMAGES/customers/meeting3.jpg'
 
 const testimonials = [
   {
     id: 1,
-    image: "https://via.placeholder.com/300x200",
+    image: cam3,
     alt: "Woman working on laptop",
   },
   {
     id: 2,
-    image: "https://via.placeholder.com/300x200",
+    image: cam1,
     alt: "Man talking to camera",
   },
   {
     id: 3,
-    image: "https://via.placeholder.com/300x200",
+    image: cam2,
+    alt: "Man recording with camera",
+  },
+];
+
+const Meetings = [
+  {
+    id: 1,
+    image: meeting3,
+    alt: "Woman working on laptop",
+  },
+  {
+    id: 2,
+    image: meeting1,
+    alt: "Man talking to camera",
+  },
+  {
+    id: 3,
+    image: meeting2,
     alt: "Man recording with camera",
   },
 ];
@@ -37,9 +62,9 @@ const CustomerTestimonials = () => {
     <h2>Our Success Stories</h2>
     <p>Hear out what our clients have to say about us.</p>
     <div className="testimonials-container">
-      {testimonials.map((testimonial) => (
-        <div key={testimonial.id} className="testimonial-card">
-          <img src={testimonial.image} alt={testimonial.alt} />
+      {Meetings.map((meeting) => (
+        <div key={meeting.id} className="testimonial-card">
+          <img src={meeting.image} alt={meeting.alt} />
           <div className="play-button">▶</div>
         </div>
       ))}
--- frontend/src/pages/impactSection/ImpactSection.jsx
+++ frontend/src/pages/impactSection/ImpactSection.jsx
@@ -15,27 +15,42 @@ const ImpactSection = () => {
 
       <div className="impact-stats">
         <div className="impact-card blue">
+          <div className="impact-card-icon">
           <FaTint className="icon" />
+          </div>
+          <div className="impact-card-content">
           <h3>13.8 crores</h3>
           <p>LITRES OF WATER</p>
+          </div>
         </div>
 
         <div className="impact-card red">
+          <div className="impact-card-icon">
           <FaBolt className="icon" />
+          </div>
+          <div className="impact-card-content">
           <h3>5,65,536</h3>
           <p>KWH OF ELECTRICITY</p>
+          </div>
         </div>
 
         <div className="impact-card yellow">
+          <div className="impact-card-icon">
           <FaOilCan className="icon" />
+          </div>
+          <div className="impact-card-content">
           <h3>2.5 lakhs</h3>
           <p>LITRES OF OIL</p>
+          </div>
         </div>
 
         <div className="impact-card green">
-          <FaTree className="icon" />
-          <h3>10,243</h3>
+          <div className="impact-card-icon">
+            <FaTree className="icon" /></div>
+          <div className="impact-card-content">
+            <h3>10,243</h3>
           <p>NUMBER OF TREES</p>
+          </div>
         </div>
       </div>
     </div>
